bad687960e3c4d146a9c104d4fb0078e9f2e8dca
Mohit Kumar <mohitkumar@Mohits-MBP.infiniasns.com>
2018-02-07 11:35:51 +0400

    First Commit to Push All to Github Account

---
+++ CIUnitTest/Test.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using Xamarin.UITest;
+using System;
+
+
+namespace CIUnitTest
+{
+    [TestFixture()]
+    public class Test
+    {
+        IApp app;
+
+        [SetUp]
+        public void Setup()
+        {
+            app = ConfigureApp.iOS.AppBundle("/Users/mohitkumar/GameChange/samples/CITest/iOS/bin/iPhoneSimulator/Debug/device-builds/iphone9.1-11.2/CITest.iOS.app").StartApp();
+        }
+
+        [Test()]
+        public void TabSwitch()
+        {
+            app.Tap(c => c.Marked("tab_feed.png"));
+            app.Tap(c => c.Marked("tab_about.png"));
+        }
+
+        [Test()]
+        public void ReplTest()
+        {
+            app.Repl();
+        }
+       
+    }
+}
